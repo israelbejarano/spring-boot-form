7ae7b3838a29d74c368b17416c56f28c82c4a1a5
israel <israel.bejarano.a@gmail.com>
2020-04-02 10:59:02 +0200

    mejora de código

--- spring-boot-form/src/main/java/com/ideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/ideas/springboot/form/app/controllers/FormController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ideas.springboot.form.app.models.domain.Usuario;
 
@@ -18,12 +17,7 @@ public class FormController {
 	}
 	
 	@PostMapping("/form")
-	public String procesar(Model model, @RequestParam String username, @RequestParam String password, @RequestParam String email) {
-		
-		Usuario usuario = new Usuario();
-		usuario.setUsername(username);
-		usuario.setEmail(email);
-		usuario.setPassword(password);
+	public String procesar(Usuario usuario, Model model) {
 		model.addAttribute("titulo", "Resultado del form");
 		model.addAttribute("usuario", usuario);
 		return "resultado";
