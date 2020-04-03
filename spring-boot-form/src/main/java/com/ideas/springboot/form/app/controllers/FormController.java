3364cd052916c12c1db2113850a2a1e994afe824
israel <israel.bejarano.a@gmail.com>
2020-04-03 10:54:56 +0200

    optimizacion de codigo

--- spring-boot-form/src/main/java/com/ideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/ideas/springboot/form/app/controllers/FormController.java
@@ -1,7 +1,5 @@
 package com.ideas.springboot.form.app.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -29,11 +27,6 @@ public class FormController {
 		
 		model.addAttribute("titulo", "Resultado del form");
 		if(result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err -> {
-				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-			});
-			model.addAttribute("error", errores);
 			return "form";
 		}
 
