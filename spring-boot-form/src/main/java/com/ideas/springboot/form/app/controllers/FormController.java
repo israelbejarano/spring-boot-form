cdfa8dc47a7b184bdc966cd192d261e186bb0e32
israel <israel.bejarano.a@gmail.com>
2020-04-04 11:23:25 +0200

    validacion personalidazada usandoclase Validation

--- spring-boot-form/src/main/java/com/ideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/ideas/springboot/form/app/controllers/FormController.java
@@ -3,6 +3,7 @@ package com.ideas.springboot.form.app.controllers;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,17 +13,21 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import com.ideas.springboot.form.app.models.domain.Usuario;
+import com.ideas.springboot.form.app.validation.UsuarioValidador;
 
 @Controller
 @SessionAttributes("usuario")
 public class FormController {
+	
+	@Autowired
+	private UsuarioValidador validador;
 	
 	@GetMapping("/form")
 	public String form(Model model) {
 		Usuario usuario = new Usuario();
 		usuario.setNombre("Juan");
 		usuario.setApellido("Tamariz");
-		usuario.setIdentificador("1234-567.89-K");
+		usuario.setIdentificador("12.567.869-K");
 		model.addAttribute("titulo", "Formulario usuario");
 		model.addAttribute("usuario", usuario);
 		return "form";
@@ -30,6 +35,8 @@ public class FormController {
 	
 	@PostMapping("/form")
 	public String procesar(@Valid Usuario usuario,BindingResult result, Model model, SessionStatus status) {
+		
+		validador.validate(usuario, result);
 		
 		model.addAttribute("titulo", "Resultado del form");
 		if(result.hasErrors()) {
