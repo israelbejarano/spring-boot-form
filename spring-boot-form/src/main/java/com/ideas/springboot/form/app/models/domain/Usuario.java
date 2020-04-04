14c6c375723254bc6bc79c6a8ec21854058bb127
israel <israel.bejarano.a@gmail.com>
2020-04-04 12:24:24 +0200

    validacion de fechas con @NotNull y @DateTimeFormat

--- spring-boot-form/src/main/java/com/ideas/springboot/form/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/ideas/springboot/form/app/models/domain/Usuario.java
@@ -1,5 +1,7 @@
 package com.ideas.springboot.form.app.models.domain;
 
+import java.util.Date;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -7,6 +9,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.ideas.springboot.form.app.validation.IdentificadorRegex;
 import com.ideas.springboot.form.app.validation.Requerido;
@@ -39,6 +43,18 @@ public class Usuario {
 	@Min(5)
 	@Max(5000)
 	private Integer cuenta;
+
+	@NotNull
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date fechaNacimiento;
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
 
 	public Integer getCuenta() {
 		return cuenta;
